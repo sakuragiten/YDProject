40b922bc4f6a73e2842288efeb20932aa07f88d7
gongsheng <gongs@dachentech.com.cn>
2018-04-19 09:00:37 +0800

    new

---
+++ YDProject/Classes/Header/YDPoject.h
@@ -0,0 +1,12 @@
+//
+//  YDPoject.h
+//  YDProject
+//
+//  Created by gongsheng on 19/04/2018.
+//
+
+#ifndef YDPoject_h
+#define YDPoject_h
+
+
+#endif /* YDPoject_h */
---
+++ YDProject/Classes/Macro/YDMacro.h
@@ -0,0 +1,12 @@
+//
+//  YDMacro.h
+//  YDProject
+//
+//  Created by gongsheng on 19/04/2018.
+//
+
+#ifndef YDMacro_h
+#define YDMacro_h
+
+
+#endif /* YDMacro_h */
